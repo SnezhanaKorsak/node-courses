26ebe08b9b4c660bb103d01a2580efb47649c9a1
SnezhanaKorsak <snezhana_korsak@mail.ru>
2022-09-08 15:19:59 +0300

    Work with the shopping cart

--- routes/card.js
+++ routes/card.js
@@ -1,31 +1,49 @@
 const { Router } = require('express')
 
-const Cart = require('../models/card')
 const Course = require('../models/course')
 
 const router = Router()
 
-router.post('/add', async (req, res) => {
-  const course = await Course.getCourseById(req.body.id)
+function mapCartItems(cart) {
+  return cart.items.map(item => ({
+    ...item.courseId._doc,
+    id: item.courseId.id,
+    count: item.count
+  }))
+}
 
-  await Cart.add(course)
+function getTotalPrice(courses) {
+  return courses.reduce((price, course) => course.price * course.count + price, 0)
+}
+
+router.post('/add', async (req, res) => {
+  const course = await Course.findById(req.body.id)
+
+  await req.user.addToCart(course)
   res.redirect('/card')
 })
 
 router.get('/', async  (req, res) => {
-  const cart = await Cart.fetch()
+  const user = await req.user.populate('cart.items.courseId')
+
+  const courses = mapCartItems(user.cart)
 
   res.render('card', {
     title: "Cart",
     isCart: true,
-    coursesInCart: cart.courses,
-    price: cart.price
+    coursesInCart: courses,
+    price: getTotalPrice(courses)
   })
 })
 
-
 router.delete('/remove/:id', async  (req, res) => {
- const cart = await Cart.remove(req.params.id)
+  await req.user.removeFromCart(req.params.id)
+  const user = await req.user.populate('cart.items.courseId')
+
+  const courses = mapCartItems(user.cart)
+  const cart = {
+    courses, price: getTotalPrice(courses)
+  }
 
   res.status(200).json(cart)
 })
