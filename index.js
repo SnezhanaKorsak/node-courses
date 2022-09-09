91a4918926e86c06f1bb84ed4d6634463fde7a98
SnezhanaKorsak <snezhana_korsak@mail.ru>
2022-09-09 11:27:56 +0300

    Add the session to database MongoDB

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-ac
 const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
 const session = require('express-session')
+const MongoStore = require('connect-mongodb-session')(session)
 
 const express = require('express')
 const path = require('path')
 
 const varMiddleware = require('./middleware/variables')
+const userMiddleware = require('./middleware/user')
 
 const homeRoutes = require('./routes/home')
 const coursesRoutes = require('./routes/courses')
@@ -15,7 +17,8 @@ const addRoutes = require('./routes/add')
 const cardRoutes = require('./routes/card')
 const ordersRoutes = require('./routes/orders')
 const authRoutes = require('./routes/auth')
-const User = require('./models/user')
+
+const MONGODB_URI = `mongodb+srv://Snega:2809fifka1103astra@cluster0.vynq4ec.mongodb.net/shop`
 
 const app = express()
 
@@ -23,6 +26,11 @@ const hbs = exphbs.create({
   defaultLayout: 'main',
   extname: 'hbs',
   handlebars: allowInsecurePrototypeAccess(Handlebars)
+})
+
+const store = new MongoStore({
+  collection: 'sessions',
+  uri: MONGODB_URI
 })
 
 app.engine('hbs', hbs.engine)
@@ -34,9 +42,11 @@ app.use(express.urlencoded({ extended: true }))
 app.use(session({
   secret: 'some secret value',
   resave: false,
-  saveUninitialized: false
+  saveUninitialized: false,
+  store
 }))
 app.use(varMiddleware)
+app.use(userMiddleware)
 
 app.use('/', homeRoutes)
 app.use('/courses', coursesRoutes)
@@ -49,20 +59,8 @@ const PORT = process.env.PORT || 3000
 
 async function start() {
   try {
-    const url = `mongodb+srv://Snega:2809fifka1103astra@cluster0.vynq4ec.mongodb.net/shop`
-    await mongoose.connect(url, { useNewUrlParser: true })
+    await mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
 
-   /* const candidate = await User.findOne()
-
-    if (!candidate) {
-      const user = new User({
-        email: 'snezhinka2809@mail.ru',
-        name: 'Snega',
-        cart: { items: [] }
-      })
-      await user.save()
-    }
-*/
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`)
     })
--- routes/courses.js
+++ routes/courses.js
@@ -1,6 +1,7 @@
 const { Router } = require('express')
 
 const Course = require('../models/course')
+const auth = require('../middleware/auth')
 
 const router = Router()
 
@@ -15,7 +16,7 @@ router.get('/', async (req, res) => {
   })
 })
 
-router.get('/:id/edit', async (req, res) => {
+router.get('/:id/edit', auth, async (req, res) => {
   const course = await Course.findById(req.params.id)
 
   if (!req.query.allow) {
@@ -28,14 +29,14 @@ router.get('/:id/edit', async (req, res) => {
   })
 })
 
-router.post('/edit', async (req, res) => {
+router.post('/edit', auth,  async (req, res) => {
   const { id, ...data } = req.body
   await Course.findByIdAndUpdate(id, data)
 
   res.redirect('/courses')
 })
 
-router.post('/remove', async (req, res) => {
+router.post('/remove', auth, async (req, res) => {
   try {
     await Course.deleteOne({ _id: req.body.id })
     res.redirect('/courses')
@@ -45,7 +46,7 @@ router.post('/remove', async (req, res) => {
 
 })
 
-router.get('/:id', async (req, res) => {
+router.get('/:id',  async (req, res) => {
   const course = await Course.findById(req.params.id)
 
   res.render('course', {
